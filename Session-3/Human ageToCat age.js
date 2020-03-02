c5f7f905bfd5804d0b7f908d97e371123bb8137b
NagamaniKambam <kambhamnagamani1995@gmail.com>
2020-03-02 10:02:41 +0530

    deleted

--- Session-3/Human ageToCat age.js
+++
@@ -1,7 +0,0 @@
-var myAge = 25;
-var earlyYears = 2;
-earlyYears *= 25;
-var laterYears= myAge - 2;
-laterYears *= 4;
-console.log("Early Years is : " + earlyYears);
-console.log("Later Years is : " + laterYears);
