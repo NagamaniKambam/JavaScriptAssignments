37f5f730d0f2c83a9621fe6ae22af714bf36a1f2
NagamaniKambam <kambhamnagamani1995@gmail.com>
2020-03-02 08:57:41 +0530

    Pushed Session-3 Programs

---
+++ Session-3/ConvertHumanAgeToCatAge.js
@@ -0,0 +1,8 @@
+var myAge = 25;
+var earlyYears = 2;
+earlyYears *= 25;
+var laterYears= myAge - 2;
+laterYears *= 4;
+myAgeInCatYears = earlyYears + laterYears;
+
+console.log('Human age is ' + myAge + ' years old which is ' + myAgeInCatYears + ' years old in cat years.');
---
+++ Session-3/Human age To Cat age.js
@@ -0,0 +1,7 @@
+var myAge = 25;
+var earlyYears = 2;
+earlyYears *= 25;
+var laterYears= myAge - 2;
+laterYears *= 4;
+console.log("Early Years is : " + earlyYears);
+console.log("Later Years is : " + laterYears);
