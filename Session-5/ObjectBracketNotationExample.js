cc2e4c56be23cf9acd0d9179706b3528287145ec
NagamaniKambam <kambhamnagamani1995@gmail.com>
2020-03-02 09:00:36 +0530

    Commited

---
+++ Session-5/ObjectBracketNotationExample.js
@@ -0,0 +1,13 @@
+// Object Creation Example Using Bracket Notation
+
+var employee = {
+    employeeId : 101,
+    employeeName : 'Smitha',
+    employeeDesignation : 'AssistantManager',
+    employeeSalary : 50000
+}
+
+console.log(employee['employeeId']);
+console.log(employee['employeeName']);
+console.log(employee['employeeDesignation']);
+console.log(employee['employeeSalary']);
---
+++ Session-5/ObjectCreationExample.js
@@ -0,0 +1,14 @@
+
+// Object Creation Example Using Dot Notation
+
+var employee = {
+    employeeId : 101,
+    employeeName : 'Smitha',
+    employeeDesignation : 'AssistantManager',
+    employeeSalary : 50000
+}
+
+console.log(employee.employeeId);
+console.log(employee.employeeName);
+console.log(employee.employeeDesignation);
+console.log(employee.employeeSalary);
